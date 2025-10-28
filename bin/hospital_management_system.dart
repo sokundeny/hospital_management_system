c92461218069675b7a37e2715cc4553cc4b0bd64
sokundeny <sokundeny18@gmail.com>
2025-10-28 12:43:24 +0700

    first commit

---
+++ bin/hospital_management_system.dart
@@ -0,0 +1,5 @@
+import 'package:hospital_management_system/hospital_management_system.dart' as hospital_management_system;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hospital_management_system.calculate()}!');
+}
---
+++ test/hospital_management_system_test.dart
@@ -0,0 +1,8 @@
+import 'package:hospital_management_system/hospital_management_system.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
